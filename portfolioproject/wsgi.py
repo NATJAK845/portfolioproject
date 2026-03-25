88dd9c430b3964cf28ac77c569711772289089d1
Nathaniel Zane Jakosalem <nathanieljakosalem38@gmail.com>
2026-03-25 10:04:43 +0800

    first commit

--- portfolioproject/wsgi.py
+++ portfolioproject/wsgi.py
@@ -6,11 +6,14 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
-
 import os
+import sys
+
+project_home = '/home/NathanielZaneJakosalem/portfolioproject'
+if project_home not in sys.path:
+    sys.path.insert(0, project_home)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'portfolioproject.settings'
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolioproject.settings')
-
 application = get_wsgi_application()
